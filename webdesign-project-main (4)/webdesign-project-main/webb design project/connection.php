84a77fb0c7d0b98feabdb2419f2179c41f1f5a62
iorvera1 <apeliorver@gmail.com>
2022-12-01 20:56:03 +0100

    Added the php connection and changed the form

---
+++ webdesign-project-main (4)/webdesign-project-main/webb design project/connection.php
@@ -0,0 +1,23 @@
+<?php
+$lastName = $_POST['lastName']
+$firstName = $_POST['firstName']
+$middleName = $_POST['middleName']
+$email = $_POST['email']
+$pass = $_POST['pass']
+$conPass = $_POST['conPass']
+
+
+//Database Connection
+$conn = new mysqli('localhost','root','','')
+if($conn->connect_error){
+    die('Connection Failed  : '.$conn->connect_error);
+}else{
+    //i gave space for the database (??)we want to insert the information too
+    $stmt = $conn->prepare("insert into ??(lastName, firstName, middleName, email)
+    values(?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssss",$lastName, $firstName, $middleName, $email);
+    $stmt->execute();
+    echo "Registration successful...";
+    $stmt->close();
+    $conn-close();
+}
