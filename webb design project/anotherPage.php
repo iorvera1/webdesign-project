0c2fd106c7a6b11e66a1931689d8d042df6023f9
Afolabi Temilola <93793646+Temilola19@users.noreply.github.com>
2022-12-17 18:33:37 +0100

    Delete webb design project directory

--- webb design project/anotherPage.php
+++
@@ -1,7 +0,0 @@
-<html>
- <body>
- Hello <?php echo $_POST["name"]; ?>!<br>
- Your mail is <?php echo $_POST["email"]; ?>.<br>
-Your password is <?php echo $_POST[“password"]; ?>.<br>
- </body>
- </html>
